a379129ed4c7e1797513d3aa6f337e8c3b561688
Joël Trousset <trousset.joel@gmail.com>
2026-09-03 02:53:10 +0200

    feat: show total ELO evol for draft session

--- src/app/component/draft-result/draft-result.ts
+++ src/app/component/draft-result/draft-result.ts
@@ -1,5 +1,5 @@
 import { Component, input } from '@angular/core';
-import { DraftSession } from 'app/model/model';
+import { DraftPlayer, DraftSession, PlayerEloChange } from 'app/model/model';
 import { RoundResult } from "../round-result/round-result";
 import { CardModule } from 'primeng/card';
 import { Debug } from "../debug/debug";
@@ -10,9 +10,11 @@ import { SplitterModule } from 'primeng/splitter';
 import { DeckTag } from "../deck-tag/deck-tag";
 
 import { TableModule } from "primeng/table";
+import _ from 'lodash';
+import { EloChange } from "../elo-change/elo-change";
 
 @Component({
-  imports: [RoundResult, PanelModule, Debug, DatePipe, TagModule, DeckTag, SplitterModule, TableModule],
+  imports: [RoundResult, PanelModule, Debug, DatePipe, TagModule, DeckTag, SplitterModule, TableModule, EloChange],
   selector: 'app-draft-result',
   styleUrl: './draft-result.css',
   templateUrl: './draft-result.html',
@@ -21,4 +23,18 @@ export class DraftResult {
 
   public draftSession = input.required<DraftSession>();
 
+
+  public totalEloChange(player: DraftPlayer, draftSession: DraftSession) {
+
+    const changes = _.chain(draftSession.games)
+      .filter(game => game.player1 === player.name || game.player2 === player.name)
+      .map(game => game.player1 === player.name ?
+        { eloChange: game.eloChange1, round: game.round } : { eloChange: game.eloChange2, round: game.round })
+
+      .value();
+
+    const before = _.minBy(changes, 'round')?.eloChange?.oldElo ?? 0;
+    const after = _.maxBy(changes, 'round')?.eloChange?.elo ?? 0;
+    return new PlayerEloChange(player.name, after, before, draftSession.date, 0);
+  }
 }
