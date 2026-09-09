49e901b337f56894abe278f93624326d9e1bd9b5
Joël Trousset <trousset.joel@gmail.com>
2026-09-09 13:14:26 +0200

    fix: player picker

--- src/app/component/player-picker/player-picker.ts
+++ src/app/component/player-picker/player-picker.ts
@@ -1,9 +1,10 @@
 import { Component, inject, model, output } from '@angular/core';
 import { SelectChangeEvent, SelectModule } from 'primeng/select';
+import { ChipModule } from 'primeng/chip';
 import { PlayerService } from 'app/service/players.service';
 import { FormsModule } from '@angular/forms';
 @Component({
-  imports: [SelectModule, FormsModule],
+  imports: [SelectModule, FormsModule, ChipModule],
   selector: 'app-player-picker',
   styleUrl: './player-picker.css',
   templateUrl: './player-picker.html',
@@ -15,5 +16,8 @@ export class PlayerPicker {
 
   public playersService = inject(PlayerService);
 
-
+  removeItem(event: MouseEvent, code: string) {
+    event.stopPropagation();
+    this.names.update(names => names.filter((c) => c !== code));
+  }
 }
